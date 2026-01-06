d33c50672d9686440ac55c0e7a931c55c4e634f9
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-06 14:03:54 +0800

    Update api.js

--- mobile-app/src/config/api.js
+++ mobile-app/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Update this with your actual backend URL
-const API_BASE_URL = 'http://localhost/iluna-studio-backend/api';
+const API_BASE_URL = 'http://172.29.16.1:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
