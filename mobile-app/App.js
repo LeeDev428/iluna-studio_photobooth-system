aeb55bc49208999df542cf9d133031ccf63435f5
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-25 12:32:07 +0800

    Update App.js

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -5,6 +5,8 @@ import LandingScreen from './src/screens/LandingScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import DashboardScreen from './src/screens/DashboardScreen';
+import TimeSlotScreen from './src/screens/TimeSlotScreen';
+import PaymentMethodScreen from './src/screens/PaymentMethodScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +23,8 @@ export default function App() {
         <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="TimeSlot" component={TimeSlotScreen} />
+        <Stack.Screen name="PaymentMethod" component={PaymentMethodScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
