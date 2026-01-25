b0d66c8092a992ba68eaee2fec440a1d95f8dc11
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-25 12:31:50 +0800

    Update api.js

--- mobile-app/src/config/api.js
+++ mobile-app/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Update this with your actual backend URL
-const API_BASE_URL = 'http://192.168.8.44:8000/api';
+const API_BASE_URL = 'http://192.168.0.31:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
