57ddac17152b48dbe71c199cd036bc546040668b
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-25 13:48:27 +0800

    Update TimeSlotScreen.js

--- mobile-app/src/screens/TimeSlotScreen.js
+++ mobile-app/src/screens/TimeSlotScreen.js
@@ -19,8 +19,8 @@ export default function TimeSlotScreen({ route, navigation }) {
   const dayOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THURS', 'FRI', 'SAT'][selectedDate.getDay()];
   
   const timeSlots = [
-    '9 10 AM', '10 11 AM', '11 12 NN', '12 1 PM', '1-2 PM',
-    '2-3 PM', '3-4 PM', '4-5 PM', '5-6 PM', '6-7 PM', '7-8 PM', '8 PM'
+    '9-10 AM', '10-11 AM', '11-12 NN', '12-1 PM', '1-2 PM',  '2-3 PM',
+    '3-4 PM', '4-5 PM', '5-6 PM', '6-7 PM', '7-8 PM', '8-9 PM'
   ];
 
   // Duration based on service type
