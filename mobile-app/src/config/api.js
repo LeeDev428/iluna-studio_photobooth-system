36798ff5f9e190cc33b0bc680b7d7f345b1f313b
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-07 00:10:09 +0800

    Update api.js

--- mobile-app/src/config/api.js
+++ mobile-app/src/config/api.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 // Use localhost for web, IP address for mobile device testing
 const API_BASE_URL = Platform.OS === 'web' 
   ? 'http://localhost:8000/api' 
-  : 'http://192.168.0.31:8000/api';
+  : 'http://192.168.0.26:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
